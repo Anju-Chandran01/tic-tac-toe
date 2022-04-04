54b5730c3ef28a91ff87a2cdd6cf73babe2b1f53
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-04-03 20:36:54 -0700

    Added : Create Tic TacToe board

---
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToeGame {
+    int Arr[] = new int[10];
+    public static void main(String[] args) {
+        System.out.println("--- Welcome to Tic Tac Toe Game ---");
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.boardCreate();
+    }
+
+    // UC1 : Create Board
+    private void boardCreate() {
+        for( int i = 1; i < 10; i++ ){
+
+        }
+    }
+}
